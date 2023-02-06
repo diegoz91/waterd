14bde1d0b1e2c8bdb73c2a610fe16d80ab214b29
diegoz91 <d.zago.1991@gmail.com>
2023-02-06 18:40:53 +0100

    Add the post section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,26 @@
 import { SparklesIcon } from '@heroicons/react/outline'
 import Input from './Input'
+import Post from './Post'
 
 const Feed = () => {
+    const posts = [{
+        id: "1",
+        name: "Gaia Riccardi",
+        username: "alealilluput",
+        userImage: "/lilliput.jpeg",
+        img: "https://images.unsplash.com/photo-1673454753170-83c796e48bd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80",
+        text: "nice view!",
+        timestamp: "2 hour ago"
+    },
+    {
+        id: "2",
+        name: "Nyco Zago",
+        username: "nyoorz",
+        userImage: "/Anoup.PNG",
+        img: "https://images.unsplash.com/photo-1675068766426-eb0f1a065314?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        text: "Wow!",
+        timestamp: "2 days ago"
+    },]
     return (
         <div className="xl:ml-[370px] border-l border-r border-gray-200  xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
             <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,6 +30,9 @@ const Feed = () => {
                 </div>
             </div>
             <Input />
+            {posts.map((post) => (
+                <Post key={post.id} post={post}/>
+            ))}
         </div>
     )
 }
