ee2432b557f7aa0a650efe306176461dcb49f819
diegoz91 <d.zago.1991@gmail.com>
2023-02-10 11:03:53 +0100

    Install next-auth and firebase initialize the firebase

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "watered-867b5.firebaseapp.com",
+  projectId: "watered-867b5",
+  storageBucket: "watered-867b5.appspot.com",
+  messagingSenderId: "705572222561",
+  appId: "1:705572222561:web:7833bd07fc459877c6dc20"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
