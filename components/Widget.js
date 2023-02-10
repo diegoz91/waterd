0834cdd5f8c8b7795e6bdacff5653b089f1c93d8
diegoz91 <d.zago.1991@gmail.com>
2023-02-10 10:24:25 +0100

    Create random users section of the widgets component

--- components/Widget.js
+++ components/Widget.js
@@ -3,7 +3,8 @@ import { useState } from 'react'
 import News from './News'
 
 const Widget = ({ newsResults, randomUserResult }) => {
-    const [articleNum, setArticleNum] = useState(3)
+    const [articleNum, setArticleNum] = useState(3);
+    const [randomUserNum, setRandomUserNum] = useState(3)
     return (
         <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
             <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
@@ -21,23 +22,23 @@ const Widget = ({ newsResults, randomUserResult }) => {
                 <button onClick={() => setArticleNum(articleNum + 3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show more</button>
             </div>
 
-            <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:-w-[75%]">
+            <div className="sticky text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:-w-[75%]">
                 <h4 className="font-bold text-xl px-4">Who to follow</h4>
-                {randomUserResult.map((randomUser) => (
-                    <div key={randomUser.login.username} className="flex items-center">
+                {randomUserResult.slice(0,randomUserNum).map((randomUser) => (
+                    <div key={randomUser.login.username} className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200">
                         <img className="rounded-full" width="40" src={randomUser.picture.thumbnail}/>
-                        <div className="">
-                            <h4>{randomUser.login.username}</h4>
-                            <h5>{randomUser.name.first + " " + randomUser.name.last}</h5>
+                        <div className="truncate ml-4 leading-5">
+                            <h4 className="font-bold hover:underline text-[14px] truncate">{randomUser.login.username}</h4>
+                            <h5 className="text-[13px] text-gray-500 truncate">{randomUser.name.first + " " + randomUser.name.last}</h5>
                         </div>
-                        <button className="">Follow</button>
+                        <button className="ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold">Follow</button>
                     </div>
                 ))}
-                <butto className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show more</butto>
+                <button onClick={() => setRandomUserNum(randomUserNum + 3)} className="text-blue-300 pl-4 pb-3 hover:text-blue-400">Show more</button>
             </div>
 
         </div>
     )
-}
+} 
 
 export default Widget
